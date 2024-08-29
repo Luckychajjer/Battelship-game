fb5bfc473f0c99d8edd1ecbfc531dd3da409d031
Luckychajjer <85841767+Luckychajjer@users.noreply.github.com>
2024-08-29 12:31:04 +0530

    Initial commit

---
+++ prod.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    mode:"production",
+    entry:"./src/script.js",
+
+    devServer:{
+        static:'./dist'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:"index.html",
+            template:"./src/index.html",
+            title : "todo app",
+            inject:'body'
+        })
+    ],
+    output:{
+        filename : "main.js",
+        path : path.resolve(__dirname,"dist"),
+        clean:true
+    }
+}
